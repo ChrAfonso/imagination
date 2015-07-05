16c9d3feb1c891ba3fe399899631943c62da24c1
Christian Afonso <chr.afonso@gmail.com>
2015-07-05 15:27:19 +0200

    Added pirate music and ship hit sound to enemy ship (TODO add in scene)

--- Assets/scripts/ShipHit.cs
+++ Assets/scripts/ShipHit.cs
@@ -7,9 +7,22 @@ public class ShipHit : MonoBehaviour {
 	int count = 20;
 	EllipsoidParticleEmitter emi;
 	bool active = false;
+
+	public AudioClip pirateMusic;
+	public AudioClip pirateMusicWin;
+
+	public AudioClip sfxHit;
+
+	private AudioSource musicPlayer;
+	private AudioSource sfxPlayer;
+
 	// Use this for initialization
 	void Start () {
-	
+		musicPlayer = gameObject.AddComponent<AudioSource>();
+		musicPlayer.clip = pirateMusic;
+		musicPlayer.Play();
+
+		sfxPlayer = gameObject.AddComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -18,7 +31,7 @@ public class ShipHit : MonoBehaviour {
 			if(hits >= 3)
 			{
 				transform.position = new Vector3(transform.position.x, transform.position.y -.3f, transform.position.z);
-				if(transform.position.y < 10)
+				if(transform.position.y < 10 && !musicPlayer.isPlaying)
 				{
 					//emi.emit = false;
 				/*
@@ -27,8 +40,8 @@ public class ShipHit : MonoBehaviour {
 					GameObject.Destroy(GameObject.Find("Small explosion(Clone)"));
 				}*/
 					GameObject.Destroy (gameObject);
-				Application.LoadLevel("main_room");
-				Toolbox.Instance.level_pirate_complete = true;
+					Application.LoadLevel("main_room");
+					Toolbox.Instance.level_pirate_complete = true;
 				}
 
 			}
@@ -61,13 +74,20 @@ public class ShipHit : MonoBehaviour {
 
 		GameObject boom = CannonFire.expl;
 		//Debug.Log ("Test");
-		
+
+		sfxPlayer.PlayOneShot(sfxHit);
+
 		boomIns = (GameObject) Instantiate (boom, coll.contacts[0].point, Quaternion.identity);
 		//Debug.Log (coll.collider.gameObject.name);
 		
 		emi = boomIns.gameObject.GetComponent<EllipsoidParticleEmitter> ();
 		emi.maxSize = 5;
 
+		if (hits >= 3)
+		{
+			musicPlayer.clip = pirateMusicWin;
+			musicPlayer.Play();
+		}
 
 	}
 
